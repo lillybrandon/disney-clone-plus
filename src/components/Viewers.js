6d6f04bcb6cbc44589cbcebb4f1c40d4b068539a
lillybrandon <blilly.bl@gmail.com>
2022-07-25 13:47:48 +0400

    ADD: Viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 function Viewers() {
   return (
-    <div>
-        Viewers
-    </div>
+    <Container>
+        <Wrap>
+            <img src="/images/viewers-disney.png" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-pixar.png" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-marvel.png" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-starwar s.png" />
+        </Wrap>
+        <Wrap>
+            <img src="/images/viewers-national.png" />
+        </Wrap>
+    </Container>
   )
 }
 
 export default Viewers
 
 
+const Container = styled.div`
+    margin-top: 30px;
+    display: grid;
+    padding: 30px 0 26px;
+    grid-gap : 25px;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+`
+
+const Wrap = styled.div`
+    border-radius: 10px;
+    cursor: pointer;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0 16px 10px -10px;
+    transition: all 250ms cubic-bezier(.25, .45, .45, .94) 0s;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8)
+    }
+`
